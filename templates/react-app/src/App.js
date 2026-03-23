765293e35bdd25e5694aadd14a5e5cd4803b5698
shubham <shubhamsrivastava12568@gmail.com>
2026-03-23 23:19:54 +0530

    React-template bugs done properly

---
+++ templates/react-app/src/App.js
@@ -0,0 +1,5 @@
+import { jsxs as _jsxs, jsx as _jsx } from "react/jsx-runtime";
+const App = ({ appName }) => {
+    return (_jsxs("div", { style: { padding: "20px", fontFamily: "sans-serif" }, children: [_jsxs("h1", { children: ["\uD83D\uDE80 Welcome to ", appName] }), _jsxs("p", { children: ["Created by ", "{{AUTHOR}}", " in ", "{{CURRENT_YEAR}}"] }), _jsx("p", { children: "Your ForgeCLI React app is ready!" })] }));
+};
+export default App;
---
+++ templates/react-app/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface AppProps {
+  appName: string;
+}
+
+const App: React.FC<AppProps> = ({ appName }) => {
+  return (
+    <div style={{ padding: "20px", fontFamily: "sans-serif" }}>
+      <h1>🚀 Welcome to {appName}</h1>
+      <p>Created by {"{{AUTHOR}}"} in {"{{CURRENT_YEAR}}"}</p>
+      <p>Your ForgeCLI React app is ready!</p>
+    </div>
+  );
+};
+
+export default App;
